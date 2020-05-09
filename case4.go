3ca420fa3a6e1a914c5c52100ac98a0548efb1a8
sumin963 <59023842+sumin963@users.noreply.github.com>
2020-05-09 15:37:46 +0900

    Create case4.go

---
+++ case4.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	// Declaration
+
+	// Template P1
+	P1 := func() {
+		// P1 Declaration
+		x1 := time.Now() // clock x1;
+		x1_1 := time.Since(x1)
+
+	Start: // Start Location
+		fmt.Println("P1_Start Location")
+		time.Sleep(time.Second * 3)
+		for {
+			x1_1 = time.Since(x1)
+			switch {
+			case x1_1 > time.Duration(5*time.Second):
+				goto Alarm
+			case x1_1 >= time.Duration(3*time.Second) && x1_1 <= time.Duration(5*time.Second):
+				time.Sleep(time.Second * time.Duration(rand.Intn(2)))
+				goto End
+			}
+		}
+
+	End: // End1 Location
+		fmt.Println("P1_End Location")
+		goto Start
+
+	Alarm:
+		fmt.Println("P1_Alarm")
+
+	}
+
+	// Template P2
+	P2 := func() {
+		// P2 Declaration
+
+	Start: // Start Location
+		fmt.Println("P2_Start Location")
+		goto End
+
+	End: // End1 Location
+		fmt.Println("P2_End Location")
+		goto Start
+	}
+
+	wg.Add(1)
+
+	// System Declaration
+	go P1()
+	go P2()
+
+	wg.Wait()
+}
